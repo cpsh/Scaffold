f28e77db96f431fd2aab901805b884015d082ea1
cpsh <langzilove@126.com>
2016-10-27 07:17:22 +0800

    添加实现方法，实现配置动态切换数据源

--- scaffold-web/src/main/java/com/scaffold/controller/SysLogController.java
+++ scaffold-web/src/main/java/com/scaffold/controller/SysLogController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.scaffold.common.util.LoggerUtil;
+import com.scaffold.annotation.DataSourceChange;
 import com.scaffold.model.SysLog;
 import com.scaffold.service.SysLogService;
 
@@ -23,16 +23,34 @@ public class SysLogController {
     @Autowired
     private SysLogService logService;
 
-    @RequestMapping(value = "/getall", method = RequestMethod.GET)
+    @RequestMapping(value = "/getMaster", method = RequestMethod.GET)
     @ResponseBody
     public Object selectSysLogs(SysLog sysLog, Integer page, Integer rows) {
         
-        logger.info(".xml : come into ........................");
-        LoggerUtil.info(".properties : come into ........................");
+//        logger.info(".xml : come into ........................");
+//        LoggerUtil.info(".properties : come into ........................");
         List<SysLog> list  = logService.selectSysLogs(0,50);
         
         return list;
-        
-        
     }
+    
+    
+    /**
+     * //在controller,service接口,实现类中均可进行切换数据源，为了细粒度控制切换的时机，建议放在接口实现类中加注解
+     * //    @DataSourceChange(slave = true)
+     * @param sysLog
+     * @param page
+     * @param rows
+     * @return
+     */
+    @RequestMapping(value = "/getSlave", method = RequestMethod.GET)
+    @ResponseBody
+    @DataSourceChange(slave = true)
+    public Object selectSlaveLogs(SysLog sysLog, Integer page, Integer rows) {
+        
+        List<SysLog> list  = logService.selectSysLogs(0,50);
+        
+        return list;
+    }
+    
 }
