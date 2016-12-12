ef87b22e512545958871d2a8b299f6314dcf8bc3
cpsh <langzilove@126.com>
2016-12-12 16:21:05 +0800

    ValidatorUtil.java
    正则表达式验证

---
+++ scaffold-common/src/main/java/com/scaffold/common/util/ValidatorUtil.java
@@ -0,0 +1,164 @@
+package com.omniprimeinc.audit.platform.autojobs.services.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 校验器：利用正则表达式校验邮箱、手机号等
+ * Created by shichp on 2016/12/10.
+ */
+public class ValidatorUtil {
+    /**
+     * 正则表达式：验证用户名
+     */
+//    public static final String REGEX_USERNAME = "^[a-zA-Z]\\w{5,17}$";
+
+    /**
+     * 正则表达式：验证密码
+     */
+//    public static final String REGEX_PASSWORD = "^[a-zA-Z0-9]{6,16}$";
+
+    /**
+     * 正则表达式：验证手机号
+     */
+    public static final String REGEX_MOBILE = "^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$";
+
+    /**
+     * 正则表达式：验证电话 - 带区号
+     */
+    public static final String REGEX_TELEPHONE_WITH_CODE = "^[0][1-9]{2,3}-[0-9]{5,10}$";
+
+    /**
+     * 正则表达式：验证电话 - 不带区号
+     */
+    public static final String REGEX_TELEPHONE_NO_CODE = "^[1-9]{1}[0-9]{5,8}$";
+
+    /**
+     * 正则表达式：验证邮箱
+     */
+    public static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+
+    /**
+     * 正则表达式：验证汉字
+     */
+    public static final String REGEX_CHINESE = "^[\u4e00-\u9fa5],{0,}$";
+
+    /**
+     * 正则表达式：验证身份证
+     */
+    public static final String REGEX_ID_CARD = "(^\\d{18}$)|(^\\d{15}$)";
+
+    /**
+     * 正则表达式：验证URL
+     */
+//    public static final String REGEX_URL = "http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?";
+
+    /**
+     * 正则表达式：验证IP地址
+     */
+    public static final String REGEX_IP_ADDR = "(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)";
+
+    /**
+     * 校验用户名
+     *
+     * @param username
+     * @return 校验通过返回true，否则返回false
+     */
+/*
+    public static boolean isUsername(String username) {
+        return Pattern.matches(REGEX_USERNAME, username);
+    }
+*/
+
+    /**
+     * 校验密码
+     *
+     * @param password
+     * @return 校验通过返回true，否则返回false
+     */
+/*
+    public static boolean isPassword(String password) {
+        return Pattern.matches(REGEX_PASSWORD, password);
+    }
+*/
+
+    /**
+     * 校验手机号
+     *
+     * @param mobile
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isMobile(String mobile) {
+        return Pattern.matches(REGEX_MOBILE, mobile);
+    }
+
+    public static boolean isTelePhone(String tel) {
+        boolean b = false;
+        if(tel.length() >9){
+            b = Pattern.matches(REGEX_TELEPHONE_WITH_CODE,tel); // 验证带区号的
+        }else{
+            b =  Pattern.matches(REGEX_TELEPHONE_NO_CODE,tel); // 验证没有区号的
+        }
+        return b;
+    }
+
+    /**
+     * 校验邮箱
+     *
+     * @param email
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isEmail(String email) {
+        return Pattern.matches(REGEX_EMAIL, email);
+    }
+
+    /**
+     * 校验汉字
+     *
+     * @param chinese
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isChinese(String chinese) {
+        return Pattern.matches(REGEX_CHINESE, chinese);
+    }
+
+    /**
+     * 校验身份证
+     *
+     * @param idCard
+     * @return 校验通过返回true，否则返回false
+     */
+    public static boolean isIDCard(String idCard) {
+        return Pattern.matches(REGEX_ID_CARD, idCard);
+    }
+
+    /**
+     * 校验URL
+     *
+     * @param url
+     * @return 校验通过返回true，否则返回false
+     */
+/*
+    public static boolean isUrl(String url) {
+        return Pattern.matches(REGEX_URL, url);
+    }
+*/
+
+    /**
+     * 校验IP地址
+     *
+     * @param ipAddr
+     * @return
+     */
+    public static boolean isIPAddr(String ipAddr) {
+        return Pattern.matches(REGEX_IP_ADDR, ipAddr);
+    }
+
+    public static void main(String[] args) {
+        String username = "fdsdfsdj";
+//        System.out.println(ValidatorUtil.isUsername(username));
+        System.out.println(ValidatorUtil.isChinese(username));
+    }
+
+
+}
